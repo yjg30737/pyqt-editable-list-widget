713757594da9c5d99ad1a2e77bdd0827e05e14bf
Jung Gyu Yoon <yjg30737@gmail.com>
2021-11-20 14:47:20 +0900

    add annotation

--- editor_list_widget/editor_list_widget.py
+++ editor_list_widget/editor_list_widget.py
@@ -10,26 +10,27 @@ class EditorListWidget(QListWidget):
     def addItem(self, item):
         super().addItem(item)
         self.setCurrentItem(item)
-        self.openPersistentEditor(item)
+        self.openPersistentEditor(item) # open the editor
         self.setFocus()
         self.__persistent_editor_activated_flag = True
 
-    def mousePressEvent(self, e):
+    def mousePressEvent(self, e): # make editor closed when user clicked somewhere else
         if self.__persistent_editor_activated_flag:
             self.closeIfPersistentEditorStillOpen()
         return super().mousePressEvent(e)
 
-    def keyPressEvent(self, e):
+    def keyPressEvent(self, e): # make editor closed when user pressed enter
         if e.key() == Qt.Key_Return:
             self.closeIfPersistentEditorStillOpen()
-            return
+            # return --> If you insert this then it won't make you consecutively add the next item.
         return super().keyPressEvent(e)
 
-    def closeIfPersistentEditorStillOpen(self):
+    def closeIfPersistentEditorStillOpen(self): # Check if user are editing item
         item = self.currentItem()
         if item:
             if self.isPersistentEditorOpen(item):
                 self.closePersistentEditor(item)
                 self.__persistent_editor_activated_flag = False
+
 
 
