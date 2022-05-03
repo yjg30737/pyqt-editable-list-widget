8c8a94bee7bce9c2d4e75e24653e3d992eec98c0
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-03 16:13:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='editor-list-widget',
+    name='pyqt-editable-list-widget',
     version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='Easily editable list widget',
-    url='https://github.com/yjg30737/editor-list-widget.git',
+    url='https://github.com/yjg30737/pyqt-editable-list-widget.git',
     install_requires=[
         'PyQt5>=5.8'
     ]
